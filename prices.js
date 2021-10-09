28352fb81e97607e1aaf27f11ba2b5693cdb4d77
Azu Aguas <azuaguas@gmail.com>
2021-10-09 17:10:18 -0500

    Add files via upload

---
+++ prices.js
@@ -0,0 +1,11 @@
+function getprices(){
+	
+	var dataSet = [
+    ["Pastel de Queso con Zarzamora","365","213","234","434"],
+["Pastel de Queso con Durazno","365","213","234","434"],
+["Pastel de Queso con Queso","365","213","234","434"],
+["Pastel de Queso con Chocolate","365","213","234","434"]
+];
+
+return dataSet;
+}
